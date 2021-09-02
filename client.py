c8016e881f616d19ad14d531c4ab1e72bb6fc69e
esthergd <62649021+esthergd@users.noreply.github.com>
2021-09-02 16:05:54 -0300

    client quase pronto

--- client.py
+++ client.py
@@ -8,8 +8,9 @@ import random
 
 
 def create_commands():
-    commands = [b"\x00",b"\x00"b"\xFF",b"\x0F",b"\xF0",b"\xFF",b"\xFF"b"\x00"]
-    n = random.randint(10,30)
+    commands = [b"\x00", b"\x00"b"\xFF", b"\x0F",
+                b"\xF0", b"\xFF", b"\xFF"b"\x00"]
+    n = random.randint(10, 30)
     flagged_commands = []
     for i in range(n):
         command = random.choice(commands)
@@ -18,31 +19,29 @@ def create_commands():
         flagged_commands.append(command)
     flagged_commands.append(b'\xAA')
     flagged_commands_bytes = b''.join(flagged_commands)
-    print(flagged_commands_bytes)
     return flagged_commands_bytes
+
 
 def main():
     try:
-        com1 = enlace('/dev/ttyACM0')
+        com1 = enlace('COM5')
 
         com1.enable()
 
         com1.sendData(b'\xcc')
 
-        txBuffer = create_commands() 
+        txBuffer = create_commands()
+        print(txBuffer)
+        com1.sendData(txBuffer)
 
-        com1.sendData(np.asarray(txBuffer))
+        time.sleep(1)
 
         com1.disable()
-
 
     except Exception as erro:
         print("ops! :-\\")
         print(erro)
         com1.disable()
-
-
-
 
 
 if __name__ == "__main__":
