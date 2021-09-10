cc6e5e3d8629ecc962e57d15ccd3d9269c5d855e
BrunoSaboya <brunobs6@al.insper.edu.br>
2021-09-10 16:07:29 -0300

    inico do projeto

--- client.py
+++ client.py
@@ -1,56 +1,24 @@
-
-
 from enlace import *
 import time
 import numpy as np
 import time
 import random
 
-
-def create_commands():
-    commands = [b"\x00", b"\x00"b"\xFF", b"\x0F",
-                b"\xF0", b"\xFF", b"\xFF"b"\x00"]
-    n = random.randint(10, 30)
-    flagged_commands = []
-    for i in range(n):
-        command = random.choice(commands)
-        if len(command) == 2:
-            flagged_commands.append(b'\xBB')
-        flagged_commands.append(command)
-    flagged_commands.append(b'\xAA')
-    flagged_commands_bytes = b''.join(flagged_commands)
-    return flagged_commands_bytes
+imagePath = 'ferro.jpg'
+imageBytes = (imagePath, 'rb').read()
 
 
-def main():
-    try:
-        com1 = enlace('COM5')
+class Pacotes:
 
-        com1.enable()
-
-        com1.sendData(b'\xcc')
-
-        txBuffer = create_commands()
-        print(txBuffer)
-        com1.sendData(txBuffer)
-
-        time.sleep(1)
-
-        while True:
-            rxBuffer, nRx = com1.getData(1)
-            if len(rxBuffer) > 0:
-                break
-
-        print(rxBuffer)
+    def __init__(self) -> None:
 
 
-        com1.disable()
-
-    except Exception as erro:
-        print("ops! :-\\")
-        print(erro)
-        com1.disable()
+        pass
 
 
-if __name__ == "__main__":
-    main()
+class Mensagem:
+
+    def __init__(self) -> None:
+
+
+        pass
