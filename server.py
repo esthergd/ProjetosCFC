fb8b32bf491ec0d5b84910cd387ed63c8a9cb67a
esthergd <62649021+esthergd@users.noreply.github.com>
2021-09-17 16:44:58 -0300

    server

--- server.py
+++ server.py
@@ -1,38 +1,105 @@
 from enlace import enlace
 import time
+import traceback
+
+
+class Pacotes():
+
+    def __init__(self, port):
+        self.com1 = enlace(port)
+        self.com1.enable()
+        print("Porta Aberta")
+        self.eop = b'\x0b\x0a\x0b\x0a'
+
+    def constroiPacotes(self, head, pacotes=b''):
+
+        return (head + pacotes + self.eop)
+
+def resend():
+    pass
+
+def constroiHead(continuar, repetir):
+    listaHead = []
+    listaHead.append(int(0).to_bytes(1, 'big'))
+    listaHead.append(int(0).to_bytes(1, 'big'))
+    listaHead.append(int(0).to_bytes(1, 'big'))
+    listaHead.append(continuar)
+    listaHead.append(repetir)
+    head = b''.join(listaHead)
+
+    while len(head) != 10:
+        head += b'\x00'
+
+    return (head)
 
 def main():
-    try:
-        com1 = enlace('/dev/ttyACM1')
-        com1.enable()
-        com1.rx.clearBuffer()
-        resultados = []
+    main = True
+    resultados = []
+    eop = b'\x0b\x0a\x0b\x0a'
+    id_previous = b''
 
-        rxBuffer, nRx = com1.getData(1)
-        print(rxBuffer)
+    pacotes = Pacotes("COM6")
 
-        while True:
-            rxBuffer, nRx = com1.getData(1)
-            if rxBuffer == b'\xAA':
-                print(rxBuffer)
-                break
-            elif rxBuffer == b'\xBB':
-                rxBuffer, nRx = com1.getData(2)
-                resultados.append(rxBuffer)
+    while main:
+
+        try:
+            totalDePacotes = 255
+            contagem = 1            
+
+            while contagem <= totalDePacotes:
+
+                rxbuffer1, nrx = pacotes.com1.getData(10)
+                tamanhoPacote = rxbuffer1[0]
+                qualPacote = rxbuffer1[1]
+                totalDePacotes = rxbuffer1[2]
+                rxbuffer2, nrx = pacotes.com1.getData(tamanhoPacote)
+                rxbuffer3, nrx = pacotes.com1.getData(4)
+                print(f'\nHEAD: {rxbuffer1}')
+                print(f'\nID DO PACOTE: {qualPacote}')
+                print(f'\nPAYLOAD: {rxbuffer2}')
+                print(f'\nEOP: {rxbuffer3}')
+
+                contagem = qualPacote
+    
+            #Handshake
+            # print("Receiving Handshake")
+            # handshake, nRx = pacotes.com1.getData(14)
+            # pacotes.com1.sendData(handshake)
+
+            #Inicia Transmissão
+
+            #Head
+            print("Receiving Head data")
+            head, headsize = pacotes.com1.getData(10)
+            id_payload = head[0] .to_bytes(1, 'big')
+            size_payload = head[1]
+            num_packages = head[2:4]
+
+            print(f"Id do pacote: {id_payload}")
+            print(f"Quantidade de pacotes: {num_packages}")
+            payload, payloadSize = pacotes.com1.getData(size_payload)
+            
+
+            EOP, nEOP = pacotes.com1.getData(4)
+            if EOP == eop:
+                print("Deu tudo certo!")
+                head = constroiHead(b'\x01', b'\x00')
+                sendNext = Pacotes.constroiPacotes(head)
+                pacotes.com1.sendData(sendNext)
+                resultados.append(payload)
+                
             else:
-                resultados.append(rxBuffer)
-        print(resultados)
+                print("Algo deu errado")
+                head = constroiHead(b'\x00', b'\x01')
+                sendAgain = Pacotes.constroiPacotes(head)
+                pacotes.com1.sendData(sendAgain)
 
-        com1.sendData(len(resultados))
 
-        time.sleep(1)
-
-        com1.disable()
-        
-    except Exception as erro:
-        print("ops! :-\\")
-        print(erro)
-        com1.disable()
+        except Exception as erro:
+            print("ops! :-\\")
+            print(traceback.format_exc())
+            print(erro)
+            pacotes.com1.disable()
 
 if __name__ == "__main__":
     main()
